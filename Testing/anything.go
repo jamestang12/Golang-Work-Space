d3764e5f7cb3c9bc815e178bcc56577dcf108106
James Tang <jamestang@Jamess-MBP.ht.home>
2020-12-22 15:50:37 -0500

    Init Commit

---
+++ Testing/anything.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Testing")
+
+	foo()
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
